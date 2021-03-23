b11a15fcd88920ce2a29c60398661e826617a1e5
shamseen <shamseen.c@gmail.com>
2021-03-23 17:35:10 -0400

    boilerplate custom element

---
+++ elements/path-leg.js
@@ -0,0 +1,26 @@
+/* Using Google web components docs
+https://developers.google.com/web/fundamentals/web-components/customelements#define */
+
+
+/* ---- 1/4th of the outer ring of spaces, 2 corners ---- */
+class PathLeg extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    /* ---- Listeners ---- */
+    // attribute is changed
+    attributeChangedCallback(attrName, oldVal, newVal) {
+
+    }
+
+    // when instance is inserted into the dom
+    connectedCallback() {
+
+    }
+
+    // when removed from dom
+    disconnectedCallback() {
+
+    }
+}
